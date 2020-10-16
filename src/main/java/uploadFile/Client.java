80499dd21aad3491e4e44f781adeacb56c15104d
����ɽ <544610616@qq.com>
2020-10-16 14:22:12 +0800

    2

--- src/main/java/uploadFile/Client.java
+++ src/main/java/uploadFile/Client.java
@@ -1,10 +1,13 @@
 package uploadFile;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
 import io.netty.handler.codec.bytes.ByteArrayEncoder;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.string.StringEncoder;
@@ -14,6 +17,7 @@ import unv.HttpDecoder;
 import unv.HttpEncoder;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -36,6 +40,13 @@ public class Client {
 //                     ch.pipeline().addLast(new HttpDecoder());
 //                     ch.pipeline().addLast(new HttpEncoder());
 //                     ch.pipeline().addLast("aggregator", new HttpObjectAggregator(10 * 1024 * 1024));
+                     ch.pipeline().addLast(new MessageToMessageEncoder(){
+                         @Override
+                         protected void encode(ChannelHandlerContext channelHandlerContext, Object o, List list) throws Exception {
+                             System.out.println("如果有解码器处理不了什么，记得最后一定要输出(这里是list.add)，不能空实现");
+                             list.add(o);
+                         }
+                     });
                      ch.pipeline().addLast(new ReadTimeoutHandler(READ_TIME_OUT));
 
                      ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
